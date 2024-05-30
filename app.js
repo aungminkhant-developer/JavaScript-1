70afb79b788b1dd489688fe3c9d6e729049f68d6
Aung Min Khant <aungminkhant.aln01@gmail.com>
2024-05-30 09:15:13 +0630

    [Complete]Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+const user = {
+  name :"Aung Min khant",
+  age : 23,
+ married : false,
+ purchases : ["phone","car","laptop"],
 
 
-const name = "Aung Min Khant";
-console.log(name);
+ sayName : function(){
+    console.log(this.name);
+}
+};
+
+user.sayName();
+
+function sayMyAge(){
+    console.log(`My Age is ${this}`);//This is window object
+}
+
+sayMyAge();
