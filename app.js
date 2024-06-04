861203b78364bbb561b97c0c86fba6d9670a7af0
Aung Min Khant <aungminkhant.aln01@gmail.com>
2024-06-04 11:22:00 +0630

    [Complete]Add style.css, update app.js and update index.html

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function sayMyAge(){
 
 sayMyAge();*/
 
-const names =["Aung","Min","Khant","Oo"];
+/*const names =["Aung","Min","Khant","Oo"];
 
 for(name of names){
     console.log(name);
@@ -33,4 +33,34 @@ while(loading<100){
     console.log("Website is still loading");
 
     loading++;
-}
+}*/
+
+/*const text = document.querySelector(".title");
+const changeColor =document.querySelector(".changeColor");
+
+text.classList.add("change");*/
+
+/*changeColor.addEventListener("click",function(){
+    text.classList.toggle("change");
+})*/
+
+const userList = document.querySelector(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn =document.querySelector(".addListBtn");
+/*for(user of userList){
+    user.addEventListener("click",function(){
+        this.style.color = "red";
+        console.log(this);
+    });
+}*/
+
+
+addListBtn.addEventListener("click" , function(){
+    //Create li out of thin air
+    const newLi = document.createElement("LI");
+    const liContent= document.createTextNode(listInput.value);
+    //Add input value inside this li
+    newLi.appendChild(liContent);
+    //Attaching the li to the user list 
+    userList.appendChild(newLi);
+});
